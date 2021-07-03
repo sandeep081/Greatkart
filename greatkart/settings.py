f7dd5a8f88ef9004657c3d9bdb79e38327ac33e5
Sandeep Jangid <67308644+sandeep081@users.noreply.github.com>
2021-07-03 13:12:58 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -147,9 +147,9 @@ USE_TZ = True
 # )
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    '/greatkart/static',
-]
+# STATICFILES_DIRS = [
+#     '/greatkart/static',
+# ]
 # STATIC_ROOT = os.path.join(BASE_DIR, '../grstatic')
 
 # media files configuration
