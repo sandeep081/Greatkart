a4e8fef996692fea9893af48aba0ae16efb740fd
Sandeep Jangid <67308644+sandeep081@users.noreply.github.com>
2021-07-03 12:08:01 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,6 +143,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'greatkart/static),
 )
 # STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
