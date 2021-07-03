7385875ba6000b978a9617f427381b01791f13ae
Sandeep Jangid <67308644+sandeep081@users.noreply.github.com>
2021-07-03 13:09:29 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,20 +138,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/sta
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'greatkart/static'),
-)
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     'greatkart/static',
-# ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'greatkart/static'),
+# )
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    '../greatkart/static',
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, '../grstatic')
 
 # media files configuration
 MEDIA_URL = '/media/'
